e6dfc4cc9c525bd8fcf2d5d4f8015d9979560d4e
rgosens2 <rgosens2@gmail.com>
2024-04-07 17:19:48 +0200

    cocktailsort OK

--- main4.cpp
+++ main4.cpp
@@ -47,6 +47,60 @@ void shuffle(int* a, int size) {
     }
 }
 
+
+// See: https://www.geeksforgeeks.org/cocktail-sort/
+void CocktailSort(int a[], int n)
+{
+    bool swapped = true;
+    int start = 0;
+    int end = n - 1;
+ 
+    while (swapped) {
+        // reset the swapped flag on entering
+        // the loop, because it might be true from
+        // a previous iteration.
+        swapped = false;
+ 
+        // loop from left to right same as
+        // the bubble sort
+        for (int i = start; i < end; ++i) {
+            if (a[i] > a[i + 1]) {
+                std::swap(a[i], a[i + 1]);
+                printArrayBar(a, n);
+                swapped = true;
+            }
+        }
+ 
+        // if nothing moved, then array is sorted.
+        if (!swapped)
+            break;
+ 
+        // otherwise, reset the swapped flag so that it
+        // can be used in the next stage
+        swapped = false;
+ 
+        // move the end point back by one, because
+        // item at the end is in its rightful spot
+        --end;
+ 
+        // from right to left, doing the
+        // same comparison as in the previous stage
+        for (int i = end - 1; i >= start; --i) {
+            if (a[i] > a[i + 1]) {
+                std::swap(a[i], a[i + 1]);
+                printArrayBar(a, n);
+                swapped = true;
+            }
+        }
+ 
+        // increase the starting point, because
+        // the last stage would have moved the next
+        // smallest number to its rightful spot.
+        ++start;
+    }
+}
+
+
 void printArrayBar(int A[], int size)
 {
     window.clear();
@@ -90,6 +144,9 @@ int main()
     std::iota(std::begin(v), std::end(v), 0); // Fill with 0, 1, ..., 99.
     v[0] = 1023;
     v[666] = 1023;
+
+    shuffle(&v[0], 1024);
+
 
     //auto rng = std::default_random_engine {};
     //std::shuffle(v.begin(), v.end(), rng); // buggy!!!
@@ -175,7 +232,8 @@ int main()
         //}
 
         // problem is we are in the game loop and have to do the sort loop as well
-        bogo_sort(&v[0], 1024);
+        //bogo_sort(&v[0], 1024);
+        CocktailSort(&v[0], 1024);
 
         //window.display();
     }
